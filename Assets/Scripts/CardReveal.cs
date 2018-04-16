911c8777f0e48f5313676358cfb006b73c5c7f1e
gamedev727 <38428019+gamedev727@users.noreply.github.com>
2018-04-16 09:08:50 -0400

    Initial game setup
    Finished setting up the level of the match two-game. The scene controller in the game implements knuth's algorithm to shuffle the card. Click on the card will display the element it contains. Clicking on it again will hide it.

---
+++ Assets/Scripts/CardReveal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardReveal : MonoBehaviour {
+
+	[SerializeField] private SceneController controller;
+
+	private bool reveal = true;
+	private int _id;
+
+	void Start () {
+		gameObject.GetComponent<SpriteRenderer> ().enabled = false; // Turns off the sprite at the start
+	}
+
+	public void OnMouseDown() {
+		gameObject.GetComponent<SpriteRenderer> ().enabled = reveal; // Turn it on and off based on mouseclick 
+		reveal = !reveal;
+	}
+
+	public int id {
+		get { return _id;}
+	}
+
+	public void SetCard (int id, Sprite image) {
+		_id = id;
+		GetComponent<SpriteRenderer> ().sprite = image; // set the sprite for this SpriteRenderer component
+	}
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+	[SerializeField] private CardReveal originalCard;
+	[SerializeField] private Sprite [] images;
+
+	public const int gridRows = 2;
+	public const int gridColumns = 4;
+	public const float offsetX = 3f;
+	public const float offsetY = 2.5f;
+
+	private CardReveal _firstRevealed;
+	private CardReveal _secondRevealed;
+		
+	void Start () {
+		Vector3 startPos = originalCard.transform.position;
+
+		int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+		numbers = ShuffleArray (numbers);
+
+		for (int i = 0; i < gridColumns; i++) {
+			for (int j = 0; j < gridRows; j++) {
+				CardReveal card;
+				if (i == 0 && j == 0) {
+					card = originalCard;
+				} else {
+					card = Instantiate (originalCard) as CardReveal;
+				}
+
+				int index = j * gridColumns + i;
+				int id = numbers [index];
+				card.SetCard (id, images [id]);
+
+				float posX = (offsetX * i) + startPos.x; // set the position of the new card
+				float posY = -(offsetY * j) + startPos.y;
+				card.transform.position = new Vector3 (posX, posY, startPos.z);
+			}
+		}	 
+	}
+
+	private int[] ShuffleArray (int [] numbers) { // Implementation of Knuth shuffle algorithm
+		int[] newArray = numbers.Clone () as int[];
+		for (int i = 0; i < newArray.Length; i++) {
+			int temp = newArray [i];
+			int replace = Random.Range (i, newArray.Length);
+			newArray [i] = newArray [replace];
+			newArray [replace] = temp;
+		}
+		return newArray;
+	}
+
+	public bool canReveal {
+		get { return _secondRevealed == null; } // getter function that returns false if there is card already revealed 
+	}
+
+	public void CardRevealed (CardReveal card) {
+	}
+}
